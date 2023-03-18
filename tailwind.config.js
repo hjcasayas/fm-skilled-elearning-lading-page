4905b93c6f01b17894d92a1b5de4eac89aa40f9c
hjcasayas <henlyjade.casayas@gmail.com>
2023-03-18 21:16:44 +0800

    feat: updated prettier and tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{js,ts,jsx,tsx}'],
-    theme: {
-        extend: {},
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      screens: {
+        xs: '480px',
+      },
+      colors: {
+        primary: '#13183F',
+        'primary-light': '#666CA3',
+        secondary: '#F74780',
+        'seconday-light': '#FFA7C3',
+        'se-gray': '#83869A',
+      },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
