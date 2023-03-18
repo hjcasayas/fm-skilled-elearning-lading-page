ff32cebcabaeb818945a6042d52940db76a281bc
hjcasayas <henlyjade.casayas@gmail.com>
2023-03-18 21:19:59 +0800

    feat: added favicon and layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import { HeaderComponent } from '@/components/header/header.component';
+import { FooterComponent } from '@/components/footer/footer.component';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/images/favicon.png" />
+      </Head>
+      <div className="w-screen h-screen">
+        <div className="container mx-auto my-0 p-4">
+          <div className="mb-9">
+            <HeaderComponent />
+          </div>
+          <Component {...pageProps} />
+        </div>
+        <FooterComponent />
+      </div>
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from 'next/head';
+import { HeaderComponent } from '@/components/header/header.component';
+import { FooterComponent } from '@/components/footer/footer.component';
+import { JumbotronComponent } from '@/components/jumbotron/jumbotron.component';
 
 export default function Home() {
-    return <p className="text-red-700">home-works!</p>
+  return (
+    <>
+      <main>
+        <JumbotronComponent />
+      </main>
+    </>
+  );
 }
